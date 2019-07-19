a3a9b373f5a0730c7fa5d15359142c29e44b8fcd
Beatriz <bccp02@gmail.com>
2019-07-19 11:56:10 -0300

    Finalizar

---
+++ main.py
@@ -0,0 +1,23 @@
+from classes import Carta, Baralho
+
+# carta1 = Carta('Paus', 'As', 1)
+# carta2 = Carta('Copas', 'As', 10)
+# carta3 = Carta('Espadas', 'As', 5)
+
+# # print(carta1)
+# # print(carta2)
+# # print(carta3)
+
+# baralho = Baralho()
+
+# print(baralho)
+# baralho.adicionar_carta(carta1)
+# baralho.adicionar_carta(carta2)
+# baralho.adicionar_carta(carta3)
+# print(baralho)
+# print(baralho.remover_carta())
+
+baralho = Baralho() 
+
+print(baralho.sortear())
+print(baralho.sortear())
